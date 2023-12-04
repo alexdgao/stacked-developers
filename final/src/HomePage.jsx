e4f2e8be9725b61bb213fbb6e2543cb654368323
alexdgao <alexdgao@berkeley.edu>
2023-12-04 14:06:49 -0800

    fullstack-final has semi-working version

---
+++ final/src/HomePage.jsx
@@ -0,0 +1,168 @@
+import React from "react";
+import "./style.css";
+
+export const HomePage = () => {
+  return (
+    <div className="home-page">
+      <div className="div">
+        <div className="navigation-bar">
+          <div className="navbar">
+            <div className="text-wrapper">login</div>
+            <div className="text-wrapper-2">blog</div>
+            <div className="text-wrapper-3">about</div>
+            <div className="text-wrapper-4">home</div>
+            <div className="logo-text">
+              <div className="overlap-group">
+                <div className="text-wrapper-5">foodies</div>
+                <div className="text-wrapper-6">berkeley</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="hello-food-lovers">
+          <div className="overlap">
+            <div className="text-wrapper-7">hello</div>
+            <div className="text-wrapper-8">lovers</div>
+            <img className="dish-removebg" alt="Dish removebg" src="/img/dish-2-removebg-preview-1.png" />
+            <img className="image" alt="Image" src="/img/image-5.png" />
+            <div className="text-wrapper-9">food</div>
+            <img className="img" alt="Image" src="/img/image-2.png" />
+            <div className="text-wrapper-8">l</div>
+            <div className="side-food-pictures">
+              <div className="overlap-group-2">
+                <div className="text-wrapper-10">week night salad</div>
+                <img className="image-2" alt="Image" src="/img/image-3.png" />
+              </div>
+              <div className="overlap-2">
+                <div className="text-wrapper-11">deconstructed</div>
+                <div className="text-wrapper-12">omelette</div>
+                <img className="image-3" alt="Image" src="/img/image-4.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="our-latest-post">
+          <div className="overlap-3">
+            <div className="text-wrapper-13">our latest</div>
+            <div className="rectangle" />
+            <div className="rectangle-2" />
+            <div className="rectangle-3" />
+            <div className="rectangle-4" />
+            <img className="arrow" alt="Arrow" src="/img/arrow-1.svg" />
+            <img className="arrow-2" alt="Arrow" src="/img/arrow-2.svg" />
+            <div className="text-wrapper-14">Title 1</div>
+            <div className="text-wrapper-15">short description</div>
+            <div className="overlap-4">
+              <div className="text-wrapper-16">read more</div>
+              <img className="line" alt="Line" src="/img/line-5.svg" />
+            </div>
+            <div className="overlap-5">
+              <div className="text-wrapper-16">read more</div>
+              <img className="line" alt="Line" src="/img/line-5.svg" />
+            </div>
+            <div className="overlap-group-3">
+              <div className="text-wrapper-16">read more</div>
+              <img className="line" alt="Line" src="/img/line-5.svg" />
+            </div>
+            <div className="text-wrapper-17">short description</div>
+            <div className="text-wrapper-18">short description</div>
+            <div className="text-wrapper-19">author, 10/01/23</div>
+            <div className="text-wrapper-20">author, 10/01/23</div>
+            <div className="text-wrapper-21">author, 10/01/23</div>
+            <div className="text-wrapper-22">Title 2</div>
+            <div className="text-wrapper-23">Title 3</div>
+            <img className="arrow-3" alt="Arrow" src="/img/arrow-5.svg" />
+            <img className="arrow-4" alt="Arrow" src="/img/arrow-5.svg" />
+            <img className="arrow-5" alt="Arrow" src="/img/arrow-5.svg" />
+          </div>
+        </div>
+        <div className="overlap-6">
+          <div className="text-blurbs">
+            <div className="who-are-we">
+              <div className="text-wrapper-24">who are we?</div>
+              <p className="welcome-to-blog-name">
+                Welcome to [Blog Name]—UC Berkeley&#39;s hub for food enthusiasts! We&#39;re a passionate group of
+                students exploring the diverse culinary scene in Berkeley and beyond.
+              </p>
+              <button className="button">
+                <div className="div-wrapper">
+                  <div className="text-wrapper-25">learn more</div>
+                </div>
+              </button>
+            </div>
+            <div className="the-hub">
+              <div className="the-hub-for-hungry">
+                the hub for
+                <br />
+                hungry people
+              </div>
+              <button className="overlap-group-wrapper">
+                <div className="div-wrapper">
+                  <div className="text-wrapper-26">all posts</div>
+                </div>
+              </button>
+              <p className="discover-quick">
+                Discover quick, delicious recipes tailored for busy UC Berkeley lifestyles. From nutritious on-the-go
+                snacks to easy weeknight dinners, our recipes are crafted for the health-conscious foodies with a
+                passion for flavor and efficiency. Let&#39;s nourish our bodies and savor every bite, even on the
+                busiest days!
+              </p>
+            </div>
+            <div className="join-the">
+              <div className="join-the-movement">
+                join the
+                <br />
+                movement!
+              </div>
+              <p className="unlock-a-world-of">
+                Unlock a world of flavors by creating your account on Berkeley Food Blog! Save your favorite recipes, share
+                your culinary creations, and stay updated on the latest tasty discoveries. Let&#39;s make your food
+                journey even more delicious together—sign up today!
+              </p>
+              <button className="button-2">
+                <div className="overlap-group-4">
+                  <div className="text-wrapper-27">login</div>
+                </div>
+              </button>
+            </div>
+          </div>
+          <div className="text-wrapper-28">image to be added</div>
+          <div className="text-wrapper-29">image to be added</div>
+          <div className="text-wrapper-30">image to be added</div>
+          <img className="image-4" alt="Image" src="/img/image-6.png" />
+          <img className="image-5" alt="Image" src="/img/image-7.png" />
+          <img className="image-6" alt="Image" src="/img/image-8.png" />
+        </div>
+        <footer className="footer">
+          <div className="logo-text-2">
+            <div className="overlap-group">
+              <div className="text-wrapper-5">foodies</div>
+              <div className="text-wrapper-6">berkeley</div>
+            </div>
+          </div>
+          <div className="footer-navigation">
+            <div className="text-wrapper-31">foodies</div>
+            <div className="text-wrapper-32">home</div>
+            <div className="text-wrapper-33">about</div>
+            <div className="text-wrapper-34">blog</div>
+            <div className="text-wrapper-35">login</div>
+          </div>
+          <div className="connect">
+            <div className="text-wrapper-31">connect</div>
+            <div className="text-wrapper-32">instagram</div>
+            <div className="text-wrapper-33">twitter</div>
+            <div className="text-wrapper-34">facebook</div>
+            <div className="text-wrapper-35">linkedin</div>
+          </div>
+          <div className="legal">
+            <div className="text-wrapper-31">legal</div>
+            <div className="text-wrapper-32">smth 1</div>
+            <div className="text-wrapper-33">smth 2</div>
+            <div className="text-wrapper-34">smth 3</div>
+          </div>
+          <img className="vector" alt="Vector" src="/img/vector.png" />
+        </footer>
+      </div>
+    </div>
+  );
+};
