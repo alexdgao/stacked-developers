920cab388b42f09ba2cb21fc269386fe98741e4f
alexdgao <alexdgao@berkeley.edu>
2023-12-04 15:43:02 -0800

    Adds About page

--- final/src/About.jsx
+++ final/src/About.jsx
@@ -1,49 +1,85 @@
-import React from 'react'
-import TopBar from "./TopBar"
-import './App.css'
+import React from "react";
+import "./style.css";
+import NavBar from "./NavBar";
 
 function About() {
-    return (
-        <>
-            <TopBar />
-            <div className="text">
-                <h1 className="centertext">About</h1>
-                <div className="article">
-                    <p>
-                        Welcome to Project 2 Blog!
-                    </p>
-                    <p>
-                        At Project 2 Blog, we believe that every student has the potential to shine, and school 
-                        clubs are the perfect platform to unlock that potential. Our mission is to provide a 
-                        comprehensive guide to all things related to school clubs, offering insights, tips, and 
-                        inspiration to help students make the most of their extracurricular experiences.
-                    </p>
-                    <p>
-                        Our team of passionate writers and educators are dedicated to bringing you the latest trends,
-                        success stories, and practical advice on how to navigate the diverse landscape of school clubs.
-                        Whether you're an aspiring leader, a creative mind, or an all-around enthusiast, we aim to 
-                        be your go-to resource for maximizing your club involvement and fostering a vibrant, 
-                        inclusive school community.
-                    </p>
-                    <p>
-                        Join us as we explore the power of collaboration, creativity, and leadership within the 
-                        dynamic world of school clubs. Let's unlock the doors to endless possibilities and create 
-                        lasting memories that will shape your academic journey and beyond.
-                    </p>
-                    <p>
-                        Feel free to reach out, share your experiences, and embark on this exciting adventure with us!
-                    </p> <br></br><br></br>
-                    <p>
-                        Stay inspired, <br></br>
-
-                        Koa Lee and Alex Gao
-                        Founder, Project 2 Blog
-                    </p>
-                </div>
-                <img className="centerimg" src="https://www.tclf.org/sites/default/files/thumbnails/image/CA_Berkeley_UniversityOfCaliforniaAtBerkeley_byCharlieNguyen-Flickr_2008_001_Sig.jpg" />
+  return (
+    <div className="about">
+      <div className="div">
+        <NavBar />
+        <footer className="footer">
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group">
+              <div className="text-wrapper-5">foodies</div>
+              <div className="text-wrapper-6">berkeley</div>
             </div>
-        </>
-    )
-}
+          </div>
+          <div className="footer-navigation">
+            <div className="text-wrapper-7">foodies</div>
+            <div className="text-wrapper-8">home</div>
+            <div className="text-wrapper-9">about</div>
+            <div className="text-wrapper-10">blog</div>
+            <div className="text-wrapper-11">login</div>
+          </div>
+          <div className="connect">
+            <div className="text-wrapper-7">connect</div>
+            <div className="text-wrapper-8">instagram</div>
+            <div className="text-wrapper-9">twitter</div>
+            <div className="text-wrapper-10">facebook</div>
+            <div className="text-wrapper-11">linkedin</div>
+          </div>
+          <div className="legal">
+            <div className="text-wrapper-7">legal</div>
+            <div className="text-wrapper-8">smth 1</div>
+            <div className="text-wrapper-9">smth 2</div>
+            <div className="text-wrapper-10">smth 3</div>
+          </div>
+          <img className="vector" alt="Vector" src="/static/img/vector.png" />
+        </footer>
+        <img
+          className="spencer-davis-vjsj"
+          alt="Spencer davis vjsj"
+          src="/static/img/spencer-davis-vjsj-hgoeg0-unsplash-1.png"
+        />
+        <div className="text-wrapper-12">about us</div>
+        <div className="overlap">
+          <div className="text-wrapper-13">yehchan yoo</div>
+          <p className="p">
+            Hi! I’m David, a third year student at UC Berkeley studying computer science. I’m always looking out for my
+            next favorite place to eat around town.
+          </p>
+        </div>
+        <p className="welcome-to-the">
+          Welcome to the Berkeley Foodies blog—where college life and food collide! We&#39;re a group of students on a
+          mission to bring you easy recipes, dorm-friendly cooking tips, and reviews of the best local eats. From
+          budget-friendly meals to indulgent treats, join us as we navigate the delicious intersection of student life
+          and gastronomy. Cheers to good food, great friends, and the unforgettable memories we create around the table!
+        </p>
+        <div className="overlap-2">
+          <p className="p">
+            Hello! My name is Yehchan – I am a UC Berkeley undergraduate studying Statistics and Political Economy while
+            exploring all the restaurants around the area. Great to meet y’all!
+          </p>
+          <div className="text-wrapper-14">alex dupeng gao</div>
+          <p className="text-wrapper-15">
+            Hello, my name is Alex. I’m a second year at UC Berkeley studying Applied Mathematics and Computer Science.
+            I have a few places I frequently visit but I’d love to try out more!
+          </p>
+        </div>
+        <div className="text-wrapper-16">shaojie zhang</div>
+        <p className="hi-there-i-m-shaojie">
+          Hi there! I&#39;m Shaojie Zhang, currently embracing the vibrant academic life at UC Berkeley where I&#39;m
+          delving into the intricate world of Data Science. Excited to connect with you all!
+        </p>
+        <div className="text-wrapper-17">haorui zhang</div>
+        <p className="greetings-i-m-haorui">
+          Greetings! I&#39;m Haorui Zhang, marking my journey through the scholarly halls of UC Berkeley studying
+          computer science. I&#39;m an avid explorer of the local food spots.
+        </p>
+        <div className="text-wrapper-18">david liu</div>
+      </div>
+    </div>
+  );
+};
 
 export default About
