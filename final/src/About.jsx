1736b0c96bf25d9312e407de4e16098535a0127d
Yeh Chan Yoo <13773473+yehchanyoo@users.noreply.github.com>
2023-12-04 17:06:10 -0800

    Removed legal

--- final/src/About.jsx
+++ final/src/About.jsx
@@ -29,12 +29,6 @@ function About() {
             <div className="text-wrapper-10">facebook</div>
             <div className="text-wrapper-11">linkedin</div>
           </div>
-          <div className="legal">
-            <div className="text-wrapper-7">legal</div>
-            <div className="text-wrapper-8">smth 1</div>
-            <div className="text-wrapper-9">smth 2</div>
-            <div className="text-wrapper-10">smth 3</div>
-          </div>
           <img className="vector" alt="Vector" src="/static/img/vector.png" />
         </footer>
         <img
