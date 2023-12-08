f98447eb724d4a1647af605f6add2bad582b447b
w3000anv <123042381+w3000anv@users.noreply.github.com>
2023-12-08 15:29:04 -0800

    Update Blog.jsx

--- final/src/Blog.jsx
+++ final/src/Blog.jsx
@@ -14,6 +14,28 @@ function Blog() {
           <img src="static/img/o-2410807706.jpg" className="blog-pic"></img>
           <Link to="/blog1" className="blog-link">The Cheese Board: A Berkeley Institution for Pizza Lovers</Link>
         </div>
+
+        <div className="horizontal-line"></div> 
+
+        <div className="main-text">
+          <img src="static/img/article1.png" className="blog-pic"></img>
+          <Link to="/blog2" className="blog-link">33-year-old Italian restaurant beloved by Berkeley students is closing</Link>
+        </div>
+
+        <div className="horizontal-line"></div> 
+
+        <div className="main-text">
+          <img src="static/img/topdog.png" className="blog-pic"></img>
+          <Link to="/blog3" className="blog-link">Berkeley institution Top Dog is on the ropes. But they still won’t take federal aid.</Link>
+        </div>
+
+        <div className="horizontal-line"></div> 
+
+        <div className="main-text">
+          <img src="static/img/La-Crepe.png" className="blog-pic"></img>
+          <Link to="/blog4" className="blog-link">Francophiles will find much to love at Berkeley’s traditional creperie</Link>
+        </div>
+
       </div>
     </div>
   );
